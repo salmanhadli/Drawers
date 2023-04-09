17709e6e5f1d17df2939b9c051bcb008f24cd074
Salman Hadli <skh174211@gmail.com>
2023-04-09 16:58:24 +0530

    code implementation

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { NavigationContainer } from "@react-navigation/native";
+import WelcomeScreen from "./screens/WelcomeScreen";
+import UserScreen from "./screens/UserScreen";
+import { StatusBar } from "expo-status-bar";
+import { Ionicons } from "@expo/vector-icons";
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        <Drawer.Navigator
+          screenOptions={{
+            headerStyle: { backgroundColor: "#3c0a6b" },
+            headerTintColor: "white",
+            drawerActiveBackgroundColor: "#f0e1ff",
+            drawerActiveTintColor: "#3c0a6b",
+            drawerStyle: {
+              // backgroundColor: "#ccc",
+            },
+          }}
+        >
+          <Drawer.Screen
+            name="Welcome"
+            component={WelcomeScreen}
+            options={{
+              drawerLabel: "Welcome Screen",
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="home" color={color} size={size} />
+              ),
+            }}
+          />
+          <Drawer.Screen
+            name="User"
+            component={UserScreen}
+            options={{
+              drawerIcon: ({ color, size }) => (
+                <Ionicons name="person" color={color} size={size} />
+              ),
+            }}
+          />
+        </Drawer.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
